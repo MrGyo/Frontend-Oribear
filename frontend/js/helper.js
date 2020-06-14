38a3d41ea52b6ae35bd8b8a724058fdb9843c285
MrGyo <61049830+MrGyo@users.noreply.github.com>
2020-06-14 13:56:06 +0200

    Badge modif

--- frontend/js/helper.js
+++ frontend/js/helper.js
@@ -50,6 +50,26 @@ function clearCart() {
     };
 }
 
+/*function clearCart() {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+      }).then((result) => {
+        if (result.value) {
+            Swal.fire(
+                'Deleted!',
+                'Your file has been deleted.',
+                'success'
+            )
+        }
+      })
+}*/
+
 
 //=== FONCTION POUR LE REFRESH DU BADGE ===//
 
