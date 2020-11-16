cdfee915c8c51e56441c8b3ce5ab70ddc8f17a23
MrGyo <61049830+MrGyo@users.noreply.github.com>
2020-11-16 17:10:28 +0100

    Add OC Backend reworked

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -6,13 +6,16 @@ let url = 'http://localhost:3000/api/teddies';
 
 async function retrieveContent(url){
     let result = await fetch(url).then(response => {
+        //=== On obtient bien un réponse de l'api avec un statut 200 ===//
         console.log(response)
         return response.json()
     })
+    //=== On retourne le résultat en provenance de l'api : un array composé de 5 objets (fiches produits) ===//
     console.log(result);
     return result;   
 }
 
+//=== Mise en place d'une boucle permettant d'afficher les fiches produits "Teddies" en utilisant la méthode createArticleHtml ===//
 retrieveContent(url).then(teddies => {
     teddies.forEach(teddy => {
         let container = document.getElementById("articles-container");
